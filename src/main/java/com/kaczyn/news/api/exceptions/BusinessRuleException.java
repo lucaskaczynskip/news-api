1bc93bc43c2251626e45e4136a1f86ac5aa7d5d1
lucaskaczynskip <lucaskaczyn@gmail.com>
2022-03-30 22:02:59 -0300

    feat: custom exception

---
+++ src/main/java/com/kaczyn/news/api/exceptions/BusinessRuleException.java
@@ -0,0 +1,8 @@
+package com.kaczyn.news.api.exceptions;
+
+public class BusinessRuleException extends Exception {
+
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
